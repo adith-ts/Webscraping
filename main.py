eecd81483e90b2a5843015e561cbac33c4f6a002
adith-ts <adithts@gmail.com>
2021-05-14 19:10:23 +0530

    displays info

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
-html_text = requests.get('https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250')
-soup = BeautifulSoup(html_text.text, 'lxml')
-series = soup.find_all('td', class_='titleColumn')
-
-
-print(series.title)
+from requests.api import get
+gname = input("Enter the series name: ")
+html_text = requests.get(f"https://www.imdb.com/find?q={gname}").text
+soup = BeautifulSoup(html_text, 'lxml')
+series= soup.find('td', class_='result_text').text
+print("INFO-->",series)
